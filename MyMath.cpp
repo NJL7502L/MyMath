a5241550983c97ab4a744038ceddf9a4ee4f8f8d
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-16 16:50:35 +0900

    整形

--- MyMath.cpp
+++ MyMath.cpp
@@ -1,6 +1,1 @@
 #include <MyMath.h>
-
-// template <typename T1, typename T2, typename T3>
-// T1 MyMath::map(T2 x, T3 in_min, T3 in_max, T1 out_min, T1 out_max) {
-  
-// }
--- MyMath.h
+++ MyMath.h
@@ -133,7 +133,7 @@ public:
   } ui64c;
 
   template <typename T1, typename T2, typename T3>
-  T1 map(T2 x, T3 in_min, T3 in_max, T1 out_min, T1 out_max){
+  T1 map(T2 x, T3 in_min, T3 in_max, T1 out_min, T1 out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   };
 };
